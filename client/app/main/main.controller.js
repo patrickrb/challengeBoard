c131cd3ec80a40e58a9931d19bb0aebebff51e4a
Patrick Burns <pburns@kineticsupply.com>
2015-10-09 14:25:26 -0500

    getting challenge data on the front end now, will loop make and make challenges its own directive soon

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,18 @@
 
 angular.module('challengeBoardApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.challenges = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/challenges').success(function(challenges) {
+      $scope.challenges = challenges;
+      socket.syncUpdates('challenge', $scope.challenges);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteChallenge = function(challenge) {
+      $http.delete('/api/challenges/' + challenge._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('challenge');
     });
   });
